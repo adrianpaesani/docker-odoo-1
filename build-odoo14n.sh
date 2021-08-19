8a0f3d75b817b7c28ce67ca41ca2c0397a949bca
rubeto <rubenfortunato@hotmail.com>
2021-08-19 16:18:20 -0300

    [IMP] remove OSHA

--- build-odoo14n.sh
+++ build-odoo14n.sh
@@ -2,9 +2,7 @@
 DOCKER_REPO=rubeto
 OVER=14
 OREL=$(date '+%Y%m%d')
-OSHA=$(git ls-remote https://github.com/odoo/odoo.git refs/heads/${OVER}.0-numa | cut -f 1)
 time docker build --no-cache=true \
     --build-arg OVER=${OVER} \
     --build-arg OREL=${OREL} \
-    --build-arg OSHA=${OSHA} \
     -t ${DOCKER_REPO}/odoo${OVER}n:${OREL} -f ./build/DockerfileN ./build 2>&1 | tee -a build-odoo${OVER}n-${OREL}.log
--- build-odoo14usa.sh
+++ build-odoo14usa.sh
@@ -2,9 +2,7 @@
 DOCKER_REPO=rubeto
 OVER=14
 OREL=$(date '+%Y%m%d')
-OSHA=$(git ls-remote https://github.com/odoo/odoo.git refs/heads/${OVER}.0-numa | cut -f 1)
 time docker build --no-cache=true \
     --build-arg OVER=${OVER} \
     --build-arg OREL=${OREL} \
-    --build-arg OSHA=${OSHA} \
     -t ${DOCKER_REPO}/odoo${OVER}usa:${OREL} -f ./build/DockerfileUSA ./build 2>&1 | tee -a build-odoo${OVER}usa-${OREL}.log
