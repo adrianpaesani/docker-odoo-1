d9b19717a9afebca74f8c40cfa009df0737d8225
rubeto <rubenfortunato@hotmail.com>
2021-09-26 18:58:26 -0300

    [UPD] updates

--- build-odoo14usa.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-DOCKER_REPO=rubeto
-OVER=14
-OREL=$(date '+%Y%m%d')
-time docker build --no-cache=true \
-    --build-arg OVER=${OVER} \
-    --build-arg OREL=${OREL} \
-    -t ${DOCKER_REPO}/odoo${OVER}usa:${OREL} -f ./build/DockerfileUSA ./build 2>&1 | tee -a build-odoo${OVER}usa-${OREL}.log
--- push-odoo14usa.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-DOCKER_REPO=rubeto
-OVER=14
-OREL=$(date '+%Y%m%d')
-time docker push ${DOCKER_REPO}/odoo${OVER}usa:${OREL}
